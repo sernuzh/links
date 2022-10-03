3c2898988d74a7b0f507cd7b7ec51aea44fe401c
sernuzh <sernuzh@gmail.com>
2022-10-03 22:51:13 +0300

    links

--- gitpull.sh
+++ gitpull.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 echo 'comon111' | sudo -S git add .
 sudo git commit -m 'links'
-sudo git pull --rebase  https://sernuzh:7f822de2205e584ade848045b59d66b8758f82f3@github.com/sernuzh/links.git  -f 
+sudo git pull --rebase  https://sernuzh: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@github.com/sernuzh/links.git  -f 
 
 #sudo chmod 777 -R FOTOKARTKA.COM.UA
 #sudo rm -rf  rebase-apply
