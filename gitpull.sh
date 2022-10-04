6b2714a2f854364f8948cd4f80b7139a887dadfb
sernuzh <sernuzh@gmail.com>
2022-10-04 08:58:38 +0300

    Delete gitpull.sh

--- gitpull.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-echo 'comon111' | sudo -S git add .
-sudo git commit -m 'links'
-sudo git pull --rebase  https://sernuzh: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@github.com/sernuzh/links.git  -f 
-
-#sudo chmod 777 -R FOTOKARTKA.COM.UA
-#sudo rm -rf  rebase-apply
