6e306879bdeb7e2380291ac7cde02aa8035c73b4
sernuzh <sernuzh@gmail.com>
2022-10-04 08:58:09 +0300

    Delete gitpush.sh

--- gitpush.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-echo 'comon111' | sudo -S git add .
-sudo git commit -m "links"
-sudo git push https://sernuzh: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@github.com/sernuzh/links.git  -f HEAD:master 
-#  виконати в папці проекту -  ./gitpush.sh
